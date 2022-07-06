c5143fbd86855c941febc6af39fda1710fc1fc32
Poulami807 <poulamiganguly10@gmail.com>
2022-07-06 16:13:35 +0530

    styling done

--- game.js
+++ game.js
@@ -1,33 +1,43 @@
 //generating random number
 var systemGuess=Math.floor(Math.random()*100)+1 //Math.random()_->randonly generates a no. between 0 and 1(exc)
-var guess=0
+var minAttempts=8
+var guess=1
+
 function guessNum(){
-    var num = document.getElementById("in").value
-    if (num === '' || num>100 || num<1){
-       console.log("Invalid input")
-    }
-    else {
-    guess++
     var input=document.getElementById("in");
-    var num = parseInt(input.value) //fetching value entered in input box
-    console.log(typeof(num))
-
-    if(num === systemGuess) {
-        console.log("Got the answer correctly")
-        document.getElementById("out").innerHTML+=" <i>Got the answer correctly</i><br>"
-    } 
-    else if(num > systemGuess) {
-         console.log("Guess a smaller number")
-         document.getElementById("out").innerHTML+=" Guess a smaller number<br>"
-
+    var num = input.value //fetching value entered in input box
+    console.log(`function call ${guess}`)
+    if(guess<=minAttempts){
+        console.log(guess)
+        if (num === '' || num>100 || num<1){
+            document.getElementById("out").innerHTML+="Invalid input<br>"
+            return
+         }
+         else {
+        guess++
+         var num = parseInt(input.value) 
+        //  console.log(typeof(num))
+         if(num === systemGuess) {
+            //  console.log("Got the answer correctly")
+             document.getElementById("out").innerHTML+=` <br><br><i>Congratulations! You guessed the number in ${guess-1} attempts</i`
+             return
+         } 
+         else if(num > systemGuess) {
+            //   console.log("Guess a smaller number")
+              document.getElementById("out").innerHTML+=" Guess a smaller number<br>"
+     
+         }
+         else {
+            //  console.log("Guess a larger number")
+             document.getElementById("out").innerHTML+=" Guess a larger number<br>"
+     
+         }
+     }
+     input.value=''
     }
-    else {
-        console.log("Guess a larger number")
-        document.getElementById("out").innerHTML+=" Guess a larger number<br>"
-
+    else{
+        document.getElementById("out").innerHTML+=`<br><br>Sorry only ${minAttempts} attempts allowed! You Lost`
     }
-}
-    
 }
 
 //innerHTML-> everything enclosed within that element including html 
